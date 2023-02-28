a0d58d17448615a1a2787ab9692c3ee286007b34
Gabriel <gabrielrengifo211@gmail.com>
2023-02-28 06:24:55 -0400

    first potential success

--- scripts.js
+++ scripts.js
@@ -19,8 +19,12 @@ class Knight {
             this.coords = location; // [x,y]
             this.x = this.coords[0]
             this.y = this.coords[1]
-            this.movements = null;
             this.board = playBoard;
+            this.steps = 0;
+            this.tracker = {
+                route: [],
+                visited: []
+            }
 
             this.board.knights.push(this)
         }
@@ -118,16 +122,42 @@ class Knight {
 
     exploreAxis(root){
         const oldLocation = this.coords;
-        this.coords = root;
+        if(root){
+            this.coords = root
+        } else {
+            this.coords = oldLocation;
+            root = oldLocation
+        }
         this.x = root[0]
         this.y = root[1]
         let a = this.exploreX();
         const b = this.exploreY()
         a = a.concat(b).sort((a,b) => a[0] - b[0]);
         this.coords = oldLocation;
+        let isRepeated = this.tracker.visited.includes(root.toString());
+        if(isRepeated == false) this.tracker.visited.push(root.toString());
+        this.sortVisited()
+        console.log('Tracker Status', this.tracker)
         return a
+    };
+    sortVisited(){
+        const zeroSort = this.tracker.visited.sort((a,b) => {
+            const f = a.split(',').map(num => parseInt(num));//first
+            const s = b.split(',').map(num => parseInt(num));//second
+            return f[0] - s[0]
+        });
+        const oneSort = zeroSort.sort((a,b) => {
+            const f = a.split(',').map(num => parseInt(num));//first
+            const s = b.split(',').map(num => parseInt(num));//second;
+            if(f[0] == s[0]){
+                return f[1] - s[1]
+            } else {
+                return f[0] - s[0]
+            }
+            
+        });
+        this.tracker.visited = oneSort;
     }
-
     populateBoard(){
         const list = {};
         let keyCounter = 1;
@@ -147,7 +177,102 @@ class Knight {
         }
         console.log(list)
         return list
-    }
+    };
+    findRoute(starting = [4,4],ending){
+        this.coords = starting; // update knight's position to the one passed
+        const macro = {
+            found: false,
+            route: null,
+            depth: null,
+            init: this.exploreAxis(starting, macro),
+            visited: [starting]
+        };
+        if(macro.init.includes(ending)) return 1;
+
+        macro.init.forEach(move => {
+            const micro = {
+                start: move,
+                depth: 1,
+                maxDepth: null,
+                route: [move],
+                best: [],
+                found: false
+            };
+            
+            while(depth < 10){
+                depth++
+                let moveSet = this.exploreAxis(micro.start, macro);
+                if(moveSet.includes(ending)) return [moveset, depth];
+                let childMoves = moveSet.map( move => {
+                    if (macro.visited.includes(move)) return [];
+                    return this.exploreAxis(move);
+                });
+                for (let i = 0; i < childMoves.length - 1; i++){
+                    if(childMoves[i].includes(ending)){
+
+                    }
+                }
+            }
+
+
+        })
+        
+    };
+    pathFinder(start,end){
+        this.steps++;
+        this.tracker.route.push(start.toString())
+        if(start == undefined || end == undefined ) {
+            console.log('A value was undefined, returning null')
+            return null;
+        }
+        if(this.steps > 64){
+            console.error('Too long, stopping');
+            return null
+        }
+        let moves = this.exploreAxis(start);
+        console.log('pathFinder Called: starting exploration for', start)
+        console.log('moves found: ', moves)
+        let found;
+
+        const verification = moves.map(arr => arr.toString());
+        const verification2 = verification.includes(end.toString());
+
+        console.log('Looking for matches, verification 1 and 2: ',verification,verification2)
+
+        if(verification2){
+            found = moves.filter(move => move.toString() == end.toString())[0];
+            console.log('Found it')
+            this.tracker.route.push(end.toString())
+            return this.tracker.route
+        } else {
+            console.log(start,'Target not here, starting exploration')
+            let childrenMoves = moves.map(move =>{
+                let a = this.exploreAxis(move);
+                console.log('childrenMoves',a,'for',move)
+                return a
+            });
+            console.log('current contents of childrenMoves:',childrenMoves)
+            console.log('STARTING: childrenMoves FILTER to remove visited ones')
+            let sorted;
+            childrenMoves.forEach(moveArr => {
+                let fArr = moveArr.filter(xyNum => {
+                        const isRepeated = this.tracker.visited.includes(xyNum.toString());
+                        //console.log(xyNum,'is Repeated?', isRepeated)
+                        return (isRepeated)? false : true;
+                    }); // Filtered Array of Moves 
+                console.log(moveArr,'ARRAY FILTERED INTO',fArr)
+                sorted = fArr.sort((a,b) => a.length - b.length)
+                console.log('SORTING', sorted)
+                if(fArr.includes(end)) {
+                    console.log('A match was found')
+                    return this.pathFinder(fArr.find(end), end)
+                }
+            });
+            console.log('Recursive Call Initiating')
+            return this.pathFinder(sorted[0], end)
+        }
+
+    };
 }
 
 class Movement{
@@ -156,5 +281,7 @@ class Movement{
     }
 }
 
-const first = new Knight([7,4]);
-first.populateBoard()
+const first = new Knight([1,1]);
+console.log(
+first.pathFinder([1,1],[4,3])
+)
