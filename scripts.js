a882a5ddb96e164789ad21166fdce20ceba6765a
Gabriel <gabrielrengifo211@gmail.com>
2023-03-02 02:56:29 -0400

    closing in

--- scripts.js
+++ scripts.js
@@ -1,15 +1,3 @@
-class Board {
-    constructor(x = 8,y = 8){
-        {
-            this.x = x;
-            this.y = y;
-            this.knights = [];
-        }
-    }
-    getBoard(){
-            return this
-        }
-}
 class Knight {
     constructor(location = [0,0], board){
         {
@@ -20,13 +8,10 @@ class Knight {
             this.steps = 0;
             this.tracker = {
                 route: [],
-                visited: []
+                visited: [],
+                found: false
             }
         }
-        console.log(
-            'A new Knight has entered the board!',
-            `Currently located at X:${this.x} Y:${this.y}`
-        )
     }
 
     exploreX(coordinates,secondMove = false){//coordinates is expected to be an array with 2 numbers, in a format of [x,y] coordinates
@@ -128,13 +113,20 @@ class Knight {
         this.sortVisited()
         console.log('Tracker Status', this.tracker)
         return a
-    };// This version is reliant on the coordinates of the Knight and uses its properties for execution
+    };// This version is reliant on the coordinates of the Knight and uses its properties for execution. Ignoring for this branchg
     predictMoves(coordinates){
         let a = this.exploreX(coordinates);
         const b = this.exploreY(coordinates);
         a = a.concat(b).sort((a,b) => a[0] - b[0]);
         return a
-    }
+    };
+    saveSort(root){
+        let isRepeated = this.tracker.visited.includes(root.toString());
+        if(isRepeated == false) this.tracker.visited.push(root.toString());
+        this.saveRoute(root);
+        this.sortVisited()
+        return this.tracker.visited
+    };
     sortVisited(){
         const zeroSort = this.tracker.visited.sort((a,b) => {
             const f = a.split(',').map(num => parseInt(num));//first
@@ -214,63 +206,69 @@ class Knight {
         
     };
     pathFinder(start,end){
-
         try {
-        this.steps++;
-        this.tracker.route.push(start.toString())
         if(start == undefined || end == undefined ) {
             console.log('A value was undefined, returning null')
             return null;
         }
+        this.steps++;
+        this.saveSort(start)
         if(this.steps > 64){
             console.error('Too long, stopping');
             return null
         }
-        let moves = this.exploreAxis(start);
+        let moves = this.predictMoves(start);
+        this.saveSort(start)
         console.log('pathFinder Called: starting exploration for', start)
-        console.log('moves found: ', moves)
+        console.log('Predictions: ', moves)
         let found;
 
-        const verification = moves.map(arr => arr.toString());
-        const verification2 = verification.includes(end.toString());
+        const foundMatch = moves.map(arr => arr.toString()).includes(end.toString());
+        console.log('is the goal found within the current predictions? ',foundMatch)
 
-        console.log('Looking for matches, verification 1 and 2: ',verification,verification2)
-
-        if(verification2){
+        if(foundMatch){
             found = moves.filter(move => move.toString() == end.toString())[0];
             console.log('Found it')
-            this.tracker.route.push(end.toString())
-            return this.tracker.route
+            this.saveSort(end.toString())
+            return tracker
         } else {
             console.log(start,'Target not here, starting exploration')
             let childrenMoves = moves.map(move =>{
-                let a = this.exploreAxis(move);
-                console.log('childrenMoves',a,'for',move)
-                return a
+                let predictions = this.predictMoves(move);
+                console.log('For',move,'Predictions',predictions)
+                return predictions
             });
             console.log('current contents of childrenMoves:',childrenMoves)
             console.log('STARTING: childrenMoves FILTER to remove visited ones')
-            let sorted;
+            let sortedByLength = [];
             childrenMoves.forEach(moveArr => {
-                let fArr = moveArr.filter(xyNum => {
+                let filteredArrayOfMoves = moveArr.filter(xyNum => {
                         const isRepeated = this.tracker.visited.includes(xyNum.toString());
                         //console.log(xyNum,'is Repeated?', isRepeated)
                         return (isRepeated)? false : true;
                     }); // Filtered Array of Moves 
-                console.log(moveArr,'ARRAY FILTERED INTO',fArr)
-                sorted = fArr.sort((a,b) => a.length - b.length)
-                console.log('SORTING', sorted)
-                if(fArr.includes(end)) {
-                    console.log('A match was found')
-                    return this.pathFinder(fArr.find(end), end)
-                }
+                console.log(moveArr,'ARRAY FILTERED INTO',filteredArrayOfMoves)
+                const stringifiedFilterArray = filteredArrayOfMoves.map(arr => arr.toString());
+                console.log('Stringified: ', stringifiedFilterArray)
+                if(stringifiedFilterArray.includes(end.toString())) {
+                    console.log('A match was found', filteredArrayOfMoves);
+                    let position = childrenMoves.indexOf(moveArr);
+                    this.saveSort(moves[position])
+                    this.saveSort(end)
+                    this.tracker.found = true;
+                    return this.tracker
+                } else
+                sortedByLength.push( filteredArrayOfMoves.sort((a,b) => a.length - b.length) )
+                console.log('A match was not found, returning sortedByLength array of possible moves sortedByLength by number of on going moves', sortedByLength);
             });
-            if (sorted.length == 0){
-                console.error('Ran out of tries.')
+            if(this.tracker.found == true) return ['Congratulations on finding a route', this.tracker];
+            if (sortedByLength.length == 0){
+                console.error('Dead end')
                 return false
             } else {
-                console.log('Recursive Call Initiating')
-                return this.pathFinder(sorted[0], end)
+                let target = sortedByLength[0];
+                console.log('Recursive Call Initiating for: ',target)
+                return this.pathFinder(target, end)
             }
             
         }
@@ -278,15 +276,76 @@ class Knight {
             console.error(error)
         }
     };
-}
+    saveRoute(num){
+        return this.tracker.route.push(num)
+    };
+    parsifyArraysOfStrings(bigArr){
+        let arrayedStrings = bigArr.map(str => str.split(','));
+        return arrayedStrings.map(array => array.map(str => parseInt(str)))
+    }
+    findRoute(start,end){//Finds a route from a given initial position towards an end position
+        if(!start || !end) return 'A value was undefined. Check for Errors';// if initial position or end position are missing, abort execution because it would throw an error on future code.
+        if(start == end) return 'A route finder is not necesary for this case.';
+        if(this.steps > 64) return 'Should not take more than 64 steps';
 
-class Movement{
-    constructor(){
+        this.steps++; // Each execution of this function is a movement of the piece.
+        this.saveSort(start);// Store the current coordinates into the 'visited' array
+        
+        const possibleMovesArr = this.predictMoves(start).map(move => move.toString());//Get an array with the coordinates to all locations to where we can move from start coordinates
 
+        const successfulRoute = ['Found a route: ',this.tracker];
+
+        if(possibleMovesArr.includes(end.toString())){
+            this.saveSort(end)
+            return successfulRoute
+        }//If end is found within the moves of start, return route
+
+        //If not, begin exploring the moves of start's possible moves
+        const ongoingPossibleMovesArr = possibleMovesArr.map(coordinates =>{
+            let parsedCoordinates = coordinates.split(',').map(str => parseInt(str));
+            let ongoingMoves = this.predictMoves(parsedCoordinates);
+            return ongoingMoves.map(arrayOfMoves => arrayOfMoves.toString())
+        });//An array that contains arrays with all the coordinates of the possible moves for the possible moves of start coordinates of this function call
+        let index = 0; // To find the position of a potential route
+        for (; index < ongoingPossibleMovesArr.length - 1; index++){
+            if(ongoingPossibleMovesArr[0].includes(end.toString())){
+                this.saveSort(possibleMovesArr[index]);
+                this.saveSort(end);
+                return successfulRoute
+            }
+        };//Loop through the coordinates and if found, save the route
+
+        //if not found yet, select from the inmediate moves the one with the least possible ongoing moves
+        
+        const ongoingByLength = this.parsifyArraysOfStrings(possibleMovesArr).map(array => {
+            return {
+                array,
+                length: this.predictMoves(array).length
+            }
+        })
+        .sort((a,b) => a.length - b.length);
+
+        let nextIndex = 0;
+        let nextCoordinate = ongoingByLength[nextIndex].array;
+        while(!this.tracker.visited.includes(nextCoordinate)){
+            if(nextIndex >= ongoingByLength.length - 1) return 'Infinite Loop Error';
+            nextIndex++;
+            nextCoordinate = ongoingByLength[nextIndex].array;
+        }
+
+
+        //return ;
+
+        console.log(start, possibleMovesArr, nextCoordinate)
+        
+        return this.findRoute(nextCoordinate, end)
     }
 }
 
+Array.prototype.str
+
 const first = new Knight([1,1]);
 console.log(
-first.predictMoves([4,4])
+first.findRoute([8,4],[8,1]),
+//first.predictMoves([2,3])
 )
